dea9ae04f40e679aba51ce755f4e5481023e3044
Vladimir Ignatev <ya.na.pochte@gmail.com>
2017-12-20 03:59:20 +0400

    renamed to markovfsm

--- setup.py
+++ setup.py
@@ -8,19 +8,23 @@ NEWS = open(os.path.join(here, 'NEWS.txt')).read()
 
 version = '0.1'
 
-install_requires = [
-    # List your project dependencies here.
-    # For more details, see:
-    # http://packages.python.org/distribute/setuptools.html#declaring-dependencies
-]
+install_requires = []
 
 
-setup(name='markov',
+setup(name='markovfsm',
     version=version,
     description="Markov Chain generator from random process data",
     long_description=README + '\n\n' + NEWS,
     classifiers=[
-      # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      'Development Status :: 3 - Alpha',
+      'Intended Audience :: Developers',
+      'Programming Language :: Python :: 2',
+      'Programming Language :: Python :: 2.6',
+      'Programming Language :: Python :: 2.7',
+      'Programming Language :: Python :: 3',
+      'Programming Language :: Python :: 3.2',
+      'Programming Language :: Python :: 3.3',
+      'Programming Language :: Python :: 3.4',
       'Topic :: Software Development :: Libraries',
       'Topic :: Scientific/Engineering :: Information Analysis',
       'Topic :: Scientific/Engineering :: Artificial Intelligence',
@@ -36,9 +40,5 @@ setup(name='markov',
     packages=find_packages('src'),
     package_dir = {'': 'src'},include_package_data=True,
     zip_safe=False,
-    install_requires=install_requires,
-    entry_points={
-        'console_scripts':
-            ['markov=markov:main']
-    }
+    install_requires=install_requires
 )
